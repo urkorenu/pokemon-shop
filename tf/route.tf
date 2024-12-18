56d1918c5e5a672549df089339b8055cee44f4e2
Ur Koren <ork14790@gmail.com>
2024-12-18 11:57:10 +0200

    Started to work on chat

--- tf/route.tf
+++ tf/route.tf
@@ -3,6 +3,8 @@ resource "aws_route53_record" "app_dns" {
   zone_id = "Z0057660LWNAO0YHSRYG"  # The ID of the hosted zone
   name    = "pika-card.store"       # The domain name
   type    = "A"                     # The record type
+  ttl     = 300                     # Time-to-live in seconds
+
 
   alias {
     name                   = aws_lb.app_lb.dns_name  # The DNS name of the load balancer
@@ -16,6 +18,8 @@ resource "aws_route53_record" "app_www_dns" {
   zone_id = "Z0057660LWNAO0YHSRYG"  # The ID of the hosted zone
   name    = "www.pika-card.store"   # The subdomain name
   type    = "A"                     # The record type
+  ttl     = 300                     # Time-to-live in seconds
+
 
   alias {
     name                   = aws_lb.app_lb.dns_name  # The DNS name of the load balancer
